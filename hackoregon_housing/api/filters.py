0f021bb0b05fbd200c109e900552811b51e36f0a
Nick Embrey <cne.embrey@gmail.com>
2019-08-25 10:41:05 -0700

    use NumberInListFilter for NcdbSampleYearly.year

--- hackoregon_housing/api/filters.py
+++ hackoregon_housing/api/filters.py
@@ -1,55 +1,40 @@
 from django_filters import rest_framework as filters
 from api.models import NcdbSampleChanges, NcdbSampleYearly, FIPSRecords, HmdaOrwa, TotalLoans
-
-from django_filters import Filter
+import coreapi
+from django_filters import Filter, NumberFilter
 from django_filters.fields import Lookup
+from django_filters.rest_framework import DjangoFilterBackend
 
-class ListFilter(Filter): # just use this for every field you need to filter multiple on
-    def filter(self, qs, value):
-        value_list = value.split(u',')
-        return super(ListFilter, self).filter(qs, Lookup(value_list, 'in'))
+class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
+    pass
 
 class NcdbSampleChangesFilter(filters.FilterSet):
-    # fips_code = filters.ModelChoiceFilter(FIPSRecords.objects.all())
-    # start_year = filters.NumberFilter()
-    # end_year = filters.NumberFilter()
-    # TODO: allow filtering by less than or equal to or more than or equal to for other fields?
 
     class Meta:
         model = NcdbSampleChanges
         fields = '__all__'
 
 class NcdbSampleYearlyFilter(filters.FilterSet):
-    # fips_code = filters.ModelChoiceFilter(FIPSRecords.objects.all())
-    # year = filters.NumberFilter(field_name='year', lookup_expr='iexact')
-    # TODO: allow filtering by less than or equal to or more than or equal to for other fields?
+
+    year = NumberInFilter(field_name='year', lookup_expr='in')
 
     class Meta:
         model = NcdbSampleYearly
         fields = '__all__'
 
 class FIPSRecordsFilter(filters.FilterSet):
-    # fips_code = filters.ModelChoiceFilter(FIPSRecords.objects.all())
-    # year = filters.NumberFilter(field_name='year', lookup_expr='iexact')
-    # TODO: allow filtering by less than or equal to or more than or equal to for other fields?
 
     class Meta:
         model = FIPSRecords
         fields = '__all__'
 
 class HmdaOrwaFilter(filters.FilterSet):
-    # fips_code = filters.ModelChoiceFilter(FIPSRecords.objects.all())
-    # year = filters.NumberFilter(field_name='year', lookup_expr='iexact')
-    # TODO: allow filtering by less than or equal to or more than or equal to for other fields?
 
     class Meta:
         model = HmdaOrwa
         fields = '__all__'
 
 class TotalLoansFilter(filters.FilterSet):
-    # fips_code = filters.ModelChoiceFilter(FIPSRecords.objects.all())
-    # year = filters.NumberFilter(field_name='year', lookup_expr='iexact')
-    # TODO: allow filtering by less than or equal to or more than or equal to for other fields?
 
     class Meta:
         model = TotalLoans
