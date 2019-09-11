c381f6a9d4708b75eb1645fb497b2dbf56e1e58b
Nick Embrey <nick.embrey@dfstudio.com>
2019-09-11 16:54:46 -0700

    Revert "and portlandhomeonwershipbyrace"
    This reverts commit 9a7fb336c08193220bc6e69cf8ad57197eaf19d4.

--- hackoregon_housing/api/models.py
+++ hackoregon_housing/api/models.py
@@ -121,19 +121,6 @@ class MultnomahHomeOwnershipByRace(models.Model):
         managed = False
         db_table = 'multnomah_home_ownership_by_race'
 
-class PortlandHomeOwnershipByRace(models.Model):
-    yr = models.FloatField(blank=True, null=True)
-    race = models.TextField(blank=True, null=True)
-    home_ownership_rate = models.FloatField(blank=True, null=True)
-    home_ownership_count = models.FloatField(blank=True, null=True)
-    household_count = models.FloatField(blank=True, null=True)
-    geoname = models.TextField(blank=True, null=True)
-    fips = models.FloatField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'portland_home_ownership_by_race'
-
 class PortlandHomeAppreciationAnnuallySince1990Ish(models.Model):
     prev_sale_year = models.TextField(blank=True, null=True)
     sale_year = models.IntegerField(blank=True, null=True)
